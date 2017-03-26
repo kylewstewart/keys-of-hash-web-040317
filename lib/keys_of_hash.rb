b515812d199f25f63112316287b4165f90260807
Kyle Stewart <kyle.w.stewart@mac.com>
2017-03-26 13:43:27 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,27 +1,13 @@
 require 'pry'
 
-def animals
-  hash = {
-    "sugar glider"=>"Australia",
-    "aye-aye"=> "Madagascar",
-    "red-footed tortoise"=>"Panama",
-    "kangaroo"=> "Australia",
-    "tomato frog"=>"Madagascar",
-    "koala"=>"Australia"
-  }
-end
-
 class Hash
   def keys_of(*arguments)
     return_array = []
     arguments.each do |argument|
       self.each do |key, value|
         return_array.push(key) if value == argument
-        # binding.pry
       end
     end
     return_array
   end
 end
-
-puts animals.keys_of("Panama", "Australia")
